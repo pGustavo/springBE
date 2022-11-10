f93d13cafb844dd493ded1f45e574acb06c1727a
Paulo Camargo <pacscam@gmail.com>
2022-11-10 22:08:55 +0000

    Added packages

--- src/main/java/com/app/sorteios/api/SorteiosApiApplication.java
+++ src/main/java/com/app/sorteios/api/SorteiosApiApplication.java
@@ -1,10 +1,10 @@
-package com.app.sorteios;
+package com.app.sorteios.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SorteiosApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
