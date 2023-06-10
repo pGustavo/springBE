204d865abf023ed1fb81e309cf07ded1472e2005
gusta <gustavo_pss@hotmail.com>
2023-06-10 01:48:54 +0100

    add configuratiuration to prevent security problem access

--- src/main/java/com/app/sorteios/api/SorteiosApiApplication.java
+++ src/main/java/com/app/sorteios/api/SorteiosApiApplication.java
@@ -2,10 +2,12 @@ package com.app.sorteios.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SorteiosApiApplication {
 
+	@CrossOrigin
 	public static void main(String[] args) {
 		SpringApplication.run(SorteiosApiApplication.class, args);
 	}
